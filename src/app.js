e9d8d789ebb87cd0734a8c43e083b7a451be5867
lonlich <30961049+lonlich@users.noreply.github.com>
2025-05-30 17:21:54 +0300

    .

---
+++ src/app.js
@@ -0,0 +1,19 @@
+// import {
+//   isTabletL,
+//   isTabletP,
+//   isPhoneL,
+//   isPhoneP,
+//   log,
+//   warn,
+//   table,
+//   pluralize,
+//   logArrayInline,
+//   logArrayColumn,
+//   logArrayList,
+//   logArrayTable,
+//   checkKeysMatch,
+//   //formatPrice,
+//   measure,
+// } from "js/utils.js";
+
+console.log('hello world');
