f56ff2091dfe91daefd522df1e587f2b8216fd6e
lonlich <30961049+lonlich@users.noreply.github.com>
2025-06-06 17:27:46 +0300

    begin file upload

--- src/upload.js
+++ src/upload.js
@@ -14,40 +14,38 @@ import url from 'node:url';
 import EventEmitter from 'node:events';
 const eventEmitter = new EventEmitter();
 import {upperCase} from 'upper-case';
+import formidable from 'formidable';
 
-/* 
-1. Создать сервер
-2. Сохранить запрошенный URL
-3. В зависимости от URL показать нужный файл
-4. В начале обработать ошибку и выдать сообщение, не обрабатывая дальнейшие ветки
-*/
 
 http.createServer((req, res) => {
-  
-  if (req.url === '/favicon.ico') {
-    res.writeHead(204);
-    return res.end();
+
+  //обработка POST-запроса (загрузка файлов)
+  if (req.method === 'POST' && req.url === '/upload') {
+    const form = formidable(); //TODO: try multiples true and false
+
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        // res.writeHead(500);
+        res.end('Ошибка при загрузке файла');
+        return;
+      };
+      log(`Поля формы: ${fields}`);
+      log(`Файлы: ${files[1]}`);
+      res.end();
+    });
   }
 
-  const stream = fs.createReadStream('./src/summer.html', 'utf-8');
-  stream.on('open', () => log('file opened...'));
-  stream.on('data', chunk => log(`Это чанк: ${chunk}`));
-  stream.on('close', () => log('file closed...'));
-  stream.on('error', err => warn(err));
-
-
+  //обработка GET-запроса (выдача html-файла)
   const reqFile = './src' + req.url + '.html';
 
   readFile(reqFile, 'utf-8')
   .then(fileContent => {
     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-    res.write(fileContent);
+    res.end(fileContent);
   })
   .catch(err => {
     warn(`ОШИБКА ЧТЕНИЯ ФАЙЛА: ${err}`);
     res.writeHead(404, { 'Content-Type': 'text/html; charset=utf-8' });
     res.end(`<h1>404, страница ${reqFile} ${upperCase('НЕ НАЙДЕНА!!!')}!</h1>`);
-});
-
-}).listen(3000, () => {log('Server running!')});
-
+  });})
+  .listen(3000, () => {log('Server running!')});
