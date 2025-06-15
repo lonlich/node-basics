4db2e9df2a029b03de1357598e4e8b70f51150c2
lonlich <30961049+lonlich@users.noreply.github.com>
2025-06-15 16:13:57 +0300

    setup node-project-template

---
+++ message-board/src/app.js
@@ -0,0 +1,70 @@
+import {
+    log,
+    warn,
+    table,
+    block,
+    fakeApiCall,
+    now
+    //formatPrice,
+} from "./js/utils.js";
+
+import {
+    __filename,
+    __dirname,
+    PORT,
+    STATIC_FOLDER_PATH,
+    PAGE404_FILE
+} from "./config.js"
+
+import express from "express";
+import expressLayouts from 'express-ejs-layouts';
+
+// import { serveHTML } from "./serveHTML.js";
+import { setupLocals } from "./middleware/setupLocals.js";
+
+import fs from "fs";
+import { access } from "fs/promises";
+import { constants } from "fs";
+import { readFile } from "fs/promises";
+import axios, { Axios } from "axios";
+import http from "node:http";
+import url from "node:url";
+import path from "path";
+import EventEmitter from "node:events";
+const eventEmitter = new EventEmitter();
+import { upperCase } from "upper-case";
+import formidable from "formidable";
+
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+//routers
+import { indexRouter } from "./routers/index-router.js";
+
+const app = express();
+
+//use EJS as template engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+//use express-ejs-layouts
+app.use(expressLayouts);
+app.set('layout', 'layout')
+
+//body parsing
+app.use(express.json());
+app.set('json spaces', 2);
+app.use(express.urlencoded({ extended: true }));
+
+//set up res.locals
+app.use(setupLocals);
+
+/* MAIN */
+
+app.use('/', indexRouter);
+
+
+//запуск сервера
+app.listen(PORT, () => {
+    log(`Server running on port ${PORT}!`);
+});
---
+++ message-board/src/routers/index-router.js
@@ -0,0 +1,51 @@
+import { log, warn, table, block } from "../js/utils.js";
+
+import {
+    __filename,
+    __dirname,
+    PORT,
+    STATIC_FOLDER_PATH,
+    PAGE404_FILE,
+} from "../config.js";
+
+import fs from "fs";
+import { access } from "fs/promises";
+import { constants } from "fs";
+import { readFile } from "fs/promises";
+import axios, { Axios } from "axios";
+import http from "node:http";
+import url from "node:url";
+import path from "path";
+import EventEmitter from "node:events";
+const eventEmitter = new EventEmitter();
+import { upperCase } from "upper-case";
+import formidable from "formidable";
+
+import { dirname } from "path";
+import { fileURLToPath } from "url"; 
+
+import express from "express";
+const app = express();
+
+app.use(express.json());
+
+export const indexRouter = express.Router();
+
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Amando",
+        added: new Date(),
+    },
+    {
+        text: "Hello World!",
+        user: "Charles",
+        added: new Date(),
+    },
+];
+
+indexRouter.get('/', (req, res) => {
+    res.render('index', {messages});
+});
+
+
